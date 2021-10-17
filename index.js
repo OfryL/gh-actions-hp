3515fa9405cc9f4567fa239d663620648c1e1387
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2021-10-17 15:41:55 +0300

    wip

--- index.js
+++ index.js
@@ -5,68 +5,75 @@ const myToken = env.GH_TOKEN || '';
 const ORG = env.GH_ORG || '';
 const WORKFLOWS_PER_REPO = 2;
 
-const serializeWorkflow = workFlows => ({
-  id: workFlows.id,
-  name: workFlows.name,
-  head_branch: workFlows.head_branch,
-  run_number: workFlows.run_number,
-  event: workFlows.event,
-  status: workFlows.status,
-  conclusion: workFlows.conclusion,
-  workflow_id: workFlows.workflow_id,
-  html_url: workFlows.html_url,
-  pull_request_number: workFlows.pull_requests.length && workFlows.pull_requests[0].number,
-  // pull_request: workFlows.pull_requests,
-  created_at: workFlows.created_at,
-  updated_at: workFlows.updated_at,
-  run_attempt: workFlows.run_attempt,
-  run_started_at: workFlows.run_started_at,
-  // head_commit: workFlows.head_commit,
-  ...(workFlows.head_commit ? {
-    head_commit_message: workFlows.head_commit.message,
-    head_commit_author_name: workFlows.head_commit.author.name,
-    head_commit_author_email: workFlows.head_commit.author.email,
+const serializeWorkflow = (repo) => workFlow => ({
+  _repo_name: repo.name,
+  _repo_full_name: repo.full_name,
+  id: workFlow.id,
+  name: workFlow.name,
+  head_branch: workFlow.head_branch,
+  run_number: workFlow.run_number,
+  event: workFlow.event,
+  status: workFlow.status,
+  conclusion: workFlow.conclusion,
+  workflow_id: workFlow.workflow_id,
+  html_url: workFlow.html_url,
+  pull_request_number: workFlow.pull_requests.length && workFlow.pull_requests[0].number,
+  // pull_request: workFlow.pull_requests,
+  created_at: workFlow.created_at,
+  updated_at: workFlow.updated_at,
+  run_attempt: workFlow.run_attempt,
+  run_started_at: workFlow.run_started_at,
+  // head_commit: workFlow.head_commit,
+  ...(workFlow.head_commit ? {
+    head_commit_message: workFlow.head_commit.message,
+    head_commit_author_name: workFlow.head_commit.author.name,
+    head_commit_author_email: workFlow.head_commit.author.email,
   } : {}),
 });
+
+const getRepos = async () => {
+  const allRes = [];
+  let page = 0;
+  let res = ['init'];
+
+  while (res && res.length !== 0) {
+    ({data: res} = await octokit.rest.repos.listForOrg({
+      org: ORG,
+      type: 'all',
+      per_page: 100,
+      page: page,
+    }));
+    page = page + 1;
+    allRes.push(...res)
+  }
+  return allRes;
+};
+
+const getWorkflowsForRepos = async function (repos, octokit) {
+  const res = {};
+  for (i = 0; i < repos.length; i++) {
+    const repo = repos[i];
+    const {name, full_name} = repo;
+    const {data: workFlows} = await octokit.rest.actions.listWorkflowRunsForRepo({
+      owner: ORG,
+      repo: name,
+      per_page: WORKFLOWS_PER_REPO,
+    });
+    if (workFlows.total_count > 0) {
+      res[full_name] = workFlows.workflow_runs.map(serializeWorkflow(repo));
+    }
+  }
+  return res;
+};
 
 async function run() {
 
   const octokit = github.getOctokit(myToken);
 
   try {
-
-    let getRepos = async () => {
-      const allRes = [];
-      let page = 0;
-      let res = ['init'];
-
-      while (res && res.length !== 0) {
-        ({data: res} = await octokit.rest.repos.listForOrg({
-          org: ORG,
-          type: 'all',
-          per_page: 100,
-          page: page,
-        }));
-        page = page + 1;
-        allRes.push(...res)
-      }
-      return allRes;
-    };
     const repos = await getRepos();
-
-    const res = {};
-    for (i = 0; i<repos.length; i++) {
-      const { name, full_name } = repos[i];
-      const {data: workFlows} = await octokit.rest.actions.listWorkflowRunsForRepo({
-        owner: ORG,
-        repo: name,
-        per_page: WORKFLOWS_PER_REPO,
-      });
-      if (workFlows.total_count > 0) {
-        res[full_name] = workFlows.workflow_runs.map(serializeWorkflow);
-      }
-    }
-    console.log(JSON.stringify({ res }));
+    const repoToWorkflows = await getWorkflowsForRepos(repos, octokit);
+    console.log(JSON.stringify({ repoToWorkflows }));
 
   } catch (e) {
     console.error(e)
